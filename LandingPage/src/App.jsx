07e7c5107534fe0de7d9c35e78807791491af9d8
quasargetter <timjanzarik@web.de>
2023-07-10 15:24:16 +0200

    push

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Footer from "./components/footer";
+import LastSlide from "./components/lastSlide";
 
 function App() {
-  return <></>;
+  return (
+   <>
+   
+  <LastSlide />
+  <Footer />
+  </>
+  );
 }
 
 export default App;
