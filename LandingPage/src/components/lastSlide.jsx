ea3546f6067d507301289c3664c2d04ca6777f9f
ad.hanif <ad.hanif@outlook.com>
2023-07-10 15:48:22 +0200

    fotter images adjusted

--- LandingPage/src/components/lastSlide.jsx
+++ LandingPage/src/components/lastSlide.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import './lastSlide.css';
-
+import React from "react";
+import "./lastSlide.css";
 
 const LastSlide = () => {
-    return (
-        <div className='images'>
-            <div className='item1'>
-                <img src="./src/images/desktop/image-gallery-milkbottles.jpg" alt="milk" />
-            </div>
-            <div className='item2'>
-                <img src="./src/images/desktop/image-gallery-orange.jpg" alt="milk" />
-            </div>
-            <div className='item3'>
-                <img src="./src/images/desktop/image-gallery-cone.jpg" alt="milk" />
-            </div>
-            <div className='item4'>
-                <img src="./src/images/desktop/image-gallery-sugarcubes.jpg" alt="milk" />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="images">
+      <img
+        src="./src/images/desktop/image-gallery-milkbottles.jpg"
+        alt="milk"
+      />
+
+      <img src="./src/images/desktop/image-gallery-orange.jpg" alt="milk" />
+
+      <img src="./src/images/desktop/image-gallery-cone.jpg" alt="milk" />
+
+      <img src="./src/images/desktop/image-gallery-sugarcubes.jpg" alt="milk" />
+    </div>
+  );
+};
 
 export default LastSlide;
